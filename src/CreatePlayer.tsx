894c236e36b6006fb6fe39814d2b6e91af30781e
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2023-05-01 13:24:55 -0300

    submit player form wip

--- src/CreatePlayer.tsx
+++ src/CreatePlayer.tsx
@@ -1,34 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {Button, Card, CardContent, Grid, TextField} from '@mui/material';
 
 
 function CreatePlayer() {
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [position, setPosition] = useState('');
+    const [shirtNum, setShirtNum] = useState('');
 
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        // Do something with the form values, e.g. send them to a server
+        console.log(name, surname, position, shirtNum);
+
+        // Clear the form values
+        setName('');
+        setSurname('');
+        setPosition('');
+        setShirtNum('');
+    };
     return (
 
         <div>
             <h1>Create Player</h1>
             <Card sx={{ width: 500 }}>
                 <CardContent>
+                    <form onSubmit={handleSubmit}>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="name" label="Name" variant="outlined"/>
+                            <TextField fullWidth id="name" label="Name" variant="outlined" value={name} onChange={(event) => setName(event.target.value)}/>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="surname" label="Surname" variant="outlined"/>
+                            <TextField fullWidth id="surname" label="Surname" variant="outlined" value={surname} onChange={(event) => setSurname(event.target.value)}/>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="position" label="Position" variant="outlined"/>
+                            <TextField fullWidth id="position" label="Position" variant="outlined" value={position} onChange={(event) => setPosition(event.target.value)}/>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="shirtNum" label="Shirt Number" variant="outlined"/>
+                            <TextField fullWidth id="shirtNum" label="Shirt Number" variant="outlined" value={shirtNum} onChange={(event) => setShirtNum(event.target.value)}/>
                         </Grid>
                         <Grid item xs={12}>
                         <Button  fullWidth variant="contained">Submit Player</Button>
                         </Grid>
                     </Grid>
+                    </form>
 
                 </CardContent>
             </Card>
