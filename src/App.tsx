fc7cb9569c6c1ff02d8fefa448994249d342463c
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2023-05-01 12:48:18 -0300

    Redirect works

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <Router>
           <Routes>
            <Route path="/" element={<Home/>} />
-           <Route path="/createplayer" element={<CreatePlayer/>} />
+           <Route path="/create-player" element={<CreatePlayer/>} />
             <Route path="*" element={<ErrorPage/>}/>
           </Routes>
 
--- src/assets/Home.tsx
+++ src/assets/Home.tsx
@@ -1,14 +1,15 @@
 import {Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 function Home() {
-
+    const navigate = useNavigate();
     return (
         <div className="App">
             <div className="App">
                 <div>
                     <h1>Basquet Scoreboard App</h1>
                 </div>
-                <Button color="secondary"  >Create Player</Button>
+                <Button color="secondary"  onClick={() => navigate('/create-player')}>Create Player</Button>
 
             </div>
 
